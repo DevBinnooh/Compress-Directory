2066c168706fa4dbdcb1a22e408ce6c46bc91c3c
DevBinnooh <devBinnooh@gmail.com>
2012-03-21 00:14:46 -0700

    Final Commit, Compressing files without zip library. (requires zlib though)

--- ArchiveIt.php
+++ ArchiveIt.php
@@ -1,13 +1,18 @@
 <?php
+/******** Includes *********/
 //var_dump(realpath(dirname(__FILE__)));
 $currentPath = realpath(dirname(__FILE__));
-require_once $currentPath.DIRECTORY_SEPARATOR."inc".DIRECTORY_SEPARATOR."Archive.php";
+require_once $currentPath.DIRECTORY_SEPARATOR."inc".DIRECTORY_SEPARATOR."Zip.php";
 
+/******* Targets *********/
 $src = $currentPath.DIRECTORY_SEPARATOR."TestInput.zip";
 $dst = $currentPath.DIRECTORY_SEPARATOR."output";
 
-$Archive = new File_Archive();
-$Archive->extract($src, $dst);
+$params = array ('add_path'=> $dst);
+
+/******** Extraction ******/
+$Archive = new Archive_Zip($src);
+$Archive->extract($params);
             
 
 ?>
